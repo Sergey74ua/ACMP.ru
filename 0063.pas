aeac119f02a47626b5f21a95de32714a567ff48d
Sergey74ua <Sergey74ua@gmail.com>
2021-10-12 21:57:31 +0300

    Create 0063.pas

---
+++ 0063.pas
@@ -0,0 +1,17 @@
+﻿program prog;
+
+var S, P, X, Y: integer;
+
+begin
+    readln(S, P);
+    
+    for X := 0 to S do
+    begin
+        Y := S - X;
+        if X * Y = P then
+        begin
+            writeln(X + ' ' + Y);
+            break
+        end;
+    end;
+end.
