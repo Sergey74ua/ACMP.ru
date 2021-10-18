7501a9d73b46f498382c55ef100ed7ce31f93649
Sergey74ua <Sergey74ua@gmail.com>
2021-10-18 22:39:16 +0300

    554 + 529

---
+++ 0529.pas
@@ -0,0 +1,11 @@
+﻿program p;
+
+var X1, Y1, X2, Y2, X, Y: integer;
+    S: real;
+begin
+    readln(X1, Y1, X2, Y2);
+    X := X2 - X1;
+    Y := Y2 - Y1;
+    S := sqrt(X * X + Y * Y);
+    write(S:0:5);
+end.
---
+++ 0554.pas
@@ -0,0 +1,13 @@
+﻿program p;
+
+var N, S, i: integer;
+
+begin
+    readln(N);
+    
+    S := 1;
+    for i := 1 to N do
+      S := S + i;
+    
+    write(S);
+end.
