c8ab077947c50c4676fbf69214419f7ac86a85ea
Sergey74ua <Sergey74ua@gmail.com>
2022-01-13 19:39:06 +0300

    Update 0328.py

--- 0328.py
+++ 0328.py
@@ -1,6 +1,8 @@
 N = int(input())
 S = 0
 for i in range(N + 1):
-    for j in range(i, N + 1):
-        S = S + i + j
+    S += i * (N - i + 1) + (i + N) * (N - i + 1) // 2
+    #for j in range(i, N + 1):
+    #    S += i + j
+##print(N * (N + 1) * (N + 2) // 2)
 print(S)
