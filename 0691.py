5f25f5aa924d2f8d61d6264f5cab59121b0793f0
Sergey74ua <Sergey74ua@gmail.com>
2022-09-24 21:44:53 +0300

    Update 0691.py

--- 0691.py
+++ 0691.py
@@ -2,7 +2,7 @@ N=int(input())
 arr=['A', 'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y']
 for i in range(N):
   s=input()
-  if (len(s)==6 and s[0] in arr) and (s[4] in arr) and (s[5] in arr) and s[1:3].isdigit():
+  if len(s)==6 and s[0] in arr and s[4] in arr and s[5] in arr and s[1:3].isdigit():
     print('Yes')
   else:
     print('No')
