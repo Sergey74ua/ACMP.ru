6939b5f0c9631e13d47fe031173a814b23fc8edb
Sergey74ua <Sergey74ua@gmail.com>
2022-09-24 19:53:31 +0300

    0691 py + pas

---
+++ 0691.pas
@@ -0,0 +1,27 @@
+﻿Program
+    p;
+Const
+    C: string = 'ABCEHKMOPTXY';
+    D: string = '0123456789';
+Var
+    N: integer;
+    S: string;
+Begin
+    Readln(N);
+    For var i := 1 to N do
+    begin
+        Readln(S);
+        If
+            (length(S)=6) and
+            (copy(S, 1, 1) in C) and
+            (copy(S, 2, 1) in D) and
+            (copy(S, 3, 1) in D) and
+            (copy(S, 4, 1) in D) and
+            (copy(S, 5, 1) in C) and
+            (copy(S, 6, 1) in C)
+        then
+            writeln('Yes')
+        else
+            writeln('No');
+    end;
+end.
