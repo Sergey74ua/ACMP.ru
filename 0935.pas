9c9d013534c4b48ec22da043cc8dbacd87bc0a3a
Sergey74ua <Sergey74ua@gmail.com>
2022-10-04 19:06:04 +0300

    0935

---
+++ 0935.pas
@@ -0,0 +1,13 @@
+﻿Program
+  P;
+Var
+  x1, y1, x2, y2: Byte;
+Begin
+  Readln(x1, y1, x2, y2);
+  If
+    (x1 + y1) mod 2 = (x2 + y2) mod 2
+  then
+    writeln('YES')
+  else
+    writeln('NO');
+end.
