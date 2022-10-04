787d92b2caa00e052483673db19b7e3c06bf3d46
Sergey74ua <Sergey74ua@gmail.com>
2022-10-04 19:42:08 +0300

    Create 0026.pas

---
+++ 0026.pas
@@ -0,0 +1,17 @@
+﻿Program
+  P;
+Var
+  x1, y1, r1, x2, y2, r2: Integer;
+  d: Real;
+Begin
+  Readln(x1, y1, r1);
+  Readln(x2, y2, r2);
+  d := sqrt(sqr(abs(x1 - x2)) + sqr(abs(y1 - y2)));
+  If
+    (r1 + r2 >= d)  and
+    (abs(r1 - r2) <= d)
+  then
+    writeln('YES')
+  else
+    writeln('NO')
+end.
