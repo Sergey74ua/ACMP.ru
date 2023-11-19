a48bd59f85f96dd35e4428651a16a6a3ac11efd8
Sergey <Sergey74ua@gmail.com>
2023-11-19 20:43:34 +0300

    Create 0649.py

---
+++ 0649.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+s = input()
+
+arr = [0]*128
+v = 0
+r = -1
+
+for l in range(0, n):
+    while r+1<n and arr[ord(s[r+1])]<k:
+        r+=1
+        arr[ord(s[r])]+=1
+    v+=(r+1-l)
+    arr[ord(s[l])]-=1
+
+print(v)
